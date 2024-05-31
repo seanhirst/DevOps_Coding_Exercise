d9e5a46a979291b0fb4154a5ce69b4eeecd0b028
DPTCSolutions <sean.s.hirst@gmail.com>
2024-05-31 08:46:27 -0400

    Update __init__.py
    def  Protocol named NodePodGrouperProtocol
    
    get_nodes() should return a dictionary 
    
    get_node(node_name) should take a node name (Node
    
    and a portage and a pair tree! hahahah

--- node_pod_grouper/__init__.py
+++ node_pod_grouper/__init__.py
@@ -1,12 +1,12 @@
-from typing import Protocol
+from typing import Protocol, List, Dict
 
 Node = str
 Pod = str
 
 
 class NodePodGrouperProtocol(Protocol):
-    def get_nodes(self) -> dict[Node, list[Pod]]:
+    def get_nodes(self) -> Dict[Node, List[Pod]]:
         ...
 
-    def get_node(self, Node) -> list[Pod]:
+    def get_node(self, node_name: Node) -> List[Pod]:
         ...
